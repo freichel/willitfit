b09580df8298962696e5ccfb28eded99789d298c
Florian Reichel <florian.reichel@gmail.com>
2021-05-30 13:14:52 +0200

    Updatind Read Me

--- willitfit/plotting/plotter.py
+++ willitfit/plotting/plotter.py
@@ -3,5 +3,6 @@ Receives 3D numeric representation of occupied space as well as article coordina
 Returns interactive 3D plot of packages
 '''
 
+from willitfit.params import VOL_INTERIOR, VOL_UNAVAILABLE, VOL_BORDER, VOL_EMPTY
 import numpy as np
 import matplotlib.pyplot as plt
