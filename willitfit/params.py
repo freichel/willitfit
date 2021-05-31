5fae19af20422238028250ce2079474cd7ed113a
Florian Reichel <florian.reichel@gmail.com>
2021-05-31 19:21:48 +0200

    Working on package placement

--- willitfit/params.py
+++ willitfit/params.py
@@ -10,4 +10,9 @@ IKEA_WEBSITE_LANGUAGE = "de" # language used, often same as IKEA_COUNTRY_DOMAIN 
 VOL_UNAVAILABLE = -1
 VOL_BORDER = 1
 VOL_INTERIOR = -2
-VOL_EMPTY = 0 
+VOL_EMPTY = 0
+
+# Function return codes
+INSUFFICIENT_SPACE = "Not enough space for packages in chosen trunk."
+INSUFFICIENT_DIMENSION = "At least one package dimension exceeds trunk dimension."
+OPT_INSUFFICIENT_SPACE = "Optimizer could not place this package (internal return code)."
