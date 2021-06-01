96f48060f1edb72da50d4f8577085e8c9a1467cd
Dominik Wagner <dominik.r.mall2@gmail.com>
2021-06-01 11:49:42 +0200

    implemented optional parameters to pass cuboid coordinates for unavailable space

--- willitfit/params.py
+++ willitfit/params.py
@@ -15,3 +15,5 @@ VOL_EMPTY = 0
 
 # Dummy data for volume array
 VOLUME_SPACE = np.zeros((40,40,40), dtype=int)
+COORDS = [["article1",0,0,0,0,10,15,10],
+          ["article2",1,10,0,0,15,10,5]]
--- willitfit/plotting/plotter.py
+++ willitfit/plotting/plotter.py
@@ -8,11 +8,11 @@ import numpy as np
 import plotly.graph_objects as go
 
 
-def generate_cuboids(article_coords):
-    '''Convert coords and dimension info into a list of plotly.go.Mesh3d objects
+def generate_cuboids(article_coords, unavailable_space=False):
+    '''Convert article start & end coords info into a list of plotly.go.Mesh3d objects
     Args:
-        article_coords - a list of articles and their start coordinates:
-        [[article_code, article_id, x_start, y_start, z_start, x_end, y_end, z_end]]
+        article_coords - a list of articles and their start coordinates: [[article_code, article_id, x_start, y_start, z_start, x_end, y_end, z_end]]
+        unavailable_space - (optional: default=False) denotes whether the generated cuboids should be treated as unavailable space
     Returns:
         meshes - a list of cuboids as Mesh3d objects
     '''
@@ -28,13 +28,16 @@ def generate_cuboids(article_coords):
         z2 = item[7] # z_end
 
         mesh = go.Mesh3d(
+            name=item[0],
             # 8 vertices of a cube
             x=[x1, x1, x2, x2, x1, x1, x2, x2],
             y=[y1, y2, y2, y1, y1, y2, y2, y1],
             z=[z1, z1, z1, z1, z2, z2, z2, z2],
-            colorscale='Jet',
+            facecolor=['grey'] if unavailable_space else None,
             alphahull=0,
-            flatshading=True
+            flatshading=True,
+            #opacity=0.5 if unavailable_space else 1,
+            hoverinfo="name",
             #i = [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
             #j = [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
             #k = [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
@@ -69,7 +72,7 @@ def draw_3d_plot(meshes):
                 type='linear',
                 range = [0,z_max],
                 showgrid=False,),
-        ),
+        )
     )
     fig = go.Figure(data=meshes, layout=layout)
     return fig
