27e831d9064afead20581c0f97d46281fdcd7e55
Florian Reichel <florian.reichel@gmail.com>
2021-06-01 16:05:58 +0200

    Adding error codes to params.py and starting work on api.py

---
+++ willitfit/api/api.py
@@ -0,0 +1,69 @@
+'''
+DOCSTRING to come
+'''
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from willitfit.params import ERRORS_OPTIMIZER, ERRORS_SCRAPER
+from willitfit.optimizers.volumeoptimizer import generate_optimizer
+from willitfit.plotting.plotter import plot_all
+import numpy as np
+
+# Initialize API
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# Decorator for interface call to API
+@app.post("/collect")
+def input_output(*args):
+    #TODO
+    # This section is for testing only
+    return args
+    
+    
+    '''
+    Receives user input from front end.
+    Calls relevant functions to process user input.
+    Returns output to front end.
+    '''
+    
+    '''
+    Unpack the following arguments
+    - article list as dict
+    - volume array
+    - IKEA website location and language (see params.py)
+    '''
+    
+    '''
+    Call scraper with article list and website location/language.
+    Receive list of package dimensions and weights.
+    '''
+    scraper_return = "TBD"
+    if scraper_return not in ERRORS_SCRAPER:
+        pass
+    else:
+        return scraper_return
+    
+    '''
+    Call optimizer with package list and volume array.
+    Receive package coordinates and filled volume array.
+    '''
+    optimizer_return = generate_optimizer(article_list, np.copy(volume_space))
+    if optimizer_return not in ERRORS_OPTIMIZER:
+        filled_space, package_coordinates = optimizer_return
+    else:
+        return optimizer_return
+    
+    '''
+    Call plotter with package coordinates and filled volume array.
+    Receive plot
+    '''
+    plotter_return = plot_all(filled_space, package_coordinates)
+    return plotter_return
--- willitfit/params.py
+++ willitfit/params.py
@@ -17,3 +17,13 @@ INSUFFICIENT_SPACE = "Not enough space for packages in chosen trunk."
 INSUFFICIENT_DIMENSION = "At least one package dimension exceeds trunk dimension."
 OPT_INSUFFICIENT_SPACE = "Optimizer could not place this package (internal return code)."
 OPT_UNSUCCESSFUL = "Optimizer was unable to place all packages."
+ERRORS_OPTIMIZER = [INSUFFICIENT_SPACE, INSUFFICIENT_DIMENSION, OPT_INSUFFICIENT_SPACE, OPT_UNSUCCESSFUL]
+
+#TODO
+'''
+Scraper section
+Please modify/enhance those error codes as needed
+'''
+WEBSITE_UNAVAILABLE = "Website temporarily unavailable."
+ARTICLE_NOT_FOUND = "One or mor articles do not exist."
+ERRORS_SCRAPER = [WEBSITE_UNAVAILABLE, ARTICLE_NOT_FOUND]
