36601f28ddff35bdf1ea7eacbbcd480c565fe9b4
Florian Reichel <florian.reichel@gmail.com>
2021-06-03 12:56:30 +0200

    Google working

--- willitfit/app_utils/googlecloud.py
+++ willitfit/app_utils/googlecloud.py
@@ -3,19 +3,45 @@ Module for interaction with Google Cloud Storage
 '''
 
 from google.cloud import storage
-from dotenv import load_dotenv
-from willitfit.params import PROJECT_DIR, ENV_FILE
+from willitfit.params import BUCKET_NAME, DATA_FOLDER, CAR_DATABASE, GOOGLE_APPLICATION_CREDENTIALS, PROJECT_DIR, PROJECT_NAME
+import pandas as pd
+import os
 
 
-def get_cloud_data():
-    # Load key
-    load_dotenv(dotenv_path=PROJECT_DIR/ENV_FILE)
+def get_cloud_data(path_to_file=f"{DATA_FOLDER}/{CAR_DATABASE}"):
+    '''
+    Returns data from chosen path as dataframe.
+    '''       
+    # Set environment variable for service account
+    set_environment_variable()
+    
+    # Read and return data
+    return pd.read_csv(f"gs://{BUCKET_NAME}/{path_to_file}")
+
+
+def send_cloud_data(path_to_file="data/test.csv"):
+    '''
+    Saves a local copy of a file, then pushes it to specified path.
+    Returns True once upload complete.
+    '''
+    # Set environment variable for service account
+    set_environment_variable()
     
     # Initiate client
     storage_client = storage.Client()
     
-    # 
-    bucket = 
+    # Specify bucket
+    bucket = storage_client.bucket(BUCKET_NAME)
     
-if __name__ == "__main__":
-    print(get_cloud_data())
+    # Create blob
+    blob = bucket.blob(path_to_file)
+    
+    # Upload blob
+    blob.upload_from_filename(PROJECT_DIR/PROJECT_NAME/path_to_file)
+    
+    return True
+
+
+def set_environment_variable():
+    # Set environment variable for service account
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(PROJECT_DIR)+"/"+GOOGLE_APPLICATION_CREDENTIALS
