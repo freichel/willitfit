ca3c8c195ad4199b79826b3d3b8e02dc0cdf4c32
Dominik Wagner <dominik.r.mall2@gmail.com>
2021-06-09 15:14:04 +0200

    explicitly get column dtypes to avoid misreading articles that start with 0s

--- willitfit/app_utils/googlecloud.py
+++ willitfit/app_utils/googlecloud.py
@@ -23,8 +23,19 @@ def get_cloud_data(path_to_file=f"{DATA_FOLDER}/{CAR_DATABASE}"):
     # Set environment variable for service account
     set_environment_variable()
 
+    # Make sure the column types are set properly
+    dtype_dict = {
+        "width": int,
+        "height": int,
+        "length": int,
+        "weight": float,
+        "packages": int,
+        "subarticle_code": str,
+        "article_code": str,
+        "product_name": str
+    }
     # Read and return data
-    return pd.read_csv(f"gs://{BUCKET_NAME}/{path_to_file}")
+    return pd.read_csv(f"gs://{BUCKET_NAME}/{path_to_file}",dtype=dtype_dict)
 
 
 def send_cloud_data(df, path_to_file="data/test.csv"):
